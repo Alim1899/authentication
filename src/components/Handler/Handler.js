94530cb1051f16ac0de3f827482136b7aec114ae
Alim1899 <alimakhchanogli@gmail.com>
2024-01-08 19:13:38 +0400

    Added firebase authentication template and firebase

---
+++ src/components/Handler/Handler.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Signup from '../Signup/Signup';
+import classes from './Handler.module.css';
+const Handler = () => {
+  return (
+    <div className={classes.main}>
+      <Signup/>
+    </div>
+  )
+}
+
+export default Handler
